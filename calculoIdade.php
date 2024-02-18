135bd0033de46953893406ad9de1c5709eb7e231
SamuelPNascimento <155584222+SamuelPNascimento@users.noreply.github.com>
2024-02-18 17:43:20 -0300

    Create calculoIdade.php

---
+++ calculoIdade.php
@@ -0,0 +1,10 @@
+<?php
+  $date =  date('Y');
+  $anoNasc = $DatNas;
+  $data = explode("-",$anoNasc);
+  $anoAno = $data[0];
+  $idade = $date - $anoAno;
+  $IdaAlu = $idade;
+?>
+
+//Esse código foi utilizado para conseguir calcular a idade utilizando a diferença de Ano Atual com o Ano de nascimento no PHP.
